1571a6f0ec690f9a8b80e486ced9e63bab7c0f5b
hg01195 <hg01195@georgiasouthern.edu>
2020-11-24 00:08:41 -0500

    Updates

---
+++ precache-manifest.54ea737cccfb623be7926e19b38b1c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4030da4b79becb85aa13783ff9d8520e",
+    "url": "/react_spa/index.html"
+  },
+  {
+    "revision": "3776d23ae651c5862865",
+    "url": "/react_spa/static/css/main.2c80d923.chunk.css"
+  },
+  {
+    "revision": "cf9337bc4abc7a29a3bb",
+    "url": "/react_spa/static/js/2.48405b42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_spa/static/js/2.48405b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3776d23ae651c5862865",
+    "url": "/react_spa/static/js/main.ad348126.chunk.js"
+  },
+  {
+    "revision": "ecb55b9bbf6327f73f5c",
+    "url": "/react_spa/static/js/runtime-main.e876babf.js"
+  }
+]);
