2f9a9dc4e16e67f6f50e3fe93c1258db707f736c
hg01195 <hg01195@georgiasouthern.edu>
2020-11-24 01:16:23 -0500

    minor changes

--- src/Main.js
+++ src/Main.js
@@ -19,15 +19,15 @@ class Main extends Component {
           <div id='stars3'></div>
           <h1>Hiya.Guha</h1>
           <ul className="header">
-            <li><Link exact="true" to="/">HOME</Link></li>
-            <li><Link to="/stuff">RESUME</Link></li>
-            <li><Link to="/contact">CONTACT</Link></li>
+            <li><Link exact="true" to="/react_spa/">HOME</Link></li>
+            <li><Link to="/react_spa/stuff">RESUME</Link></li>
+            <li><Link to="/react_spa/contact">CONTACT</Link></li>
           </ul>
           <div className="content">
             <Switch>
-              <Route path="/stuff" component={Stuff} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/" component={Home} />
+              <Route path="/react_spa/stuff" component={Stuff} />
+              <Route path="/react_spa/contact" component={Contact} />
+              <Route path="/react_spa/" component={Home} />
             </Switch>
           </div>
           <br>
--- src/Stuff.js
+++ src/Stuff.js
@@ -5,7 +5,7 @@ class Stuff extends Component {
     return (
       <div>
         <h2>Fullstack Developer</h2>
-        <a href="https://media-exp1.licdn.com/dms/document/C4D2DAQGT3SOV18ly0g/profile-treasury-document-pdf-analyzed/0/1606197276027?e=1606284000&v=beta&t=n7SS-wuOBDZx6tye8nTXVN_c9C6co6Y1qMXLLX8iwhM">resume</a>.
+        <p><a href="https://media-exp1.licdn.com/dms/document/C4D2DAQGT3SOV18ly0g/profile-treasury-document-pdf-analyzed/0/1606197276027?e=1606284000&v=beta&t=n7SS-wuOBDZx6tye8nTXVN_c9C6co6Y1qMXLLX8iwhM">RESUME</a></p>
       </div>
     );
   }
